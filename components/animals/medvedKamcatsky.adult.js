aabebe3c048df096c48a099bdfc952d4bb8f5e4e
Marek 'marx' Grác <mgrac@redhat.com>
2016-10-17 12:26:14 +0200

    Animals update

--- components/animals/medvedKamcatsky.adult.js
+++ components/animals/medvedKamcatsky.adult.js
@@ -1,101 +1,60 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   View,
   Text,
   ScrollView,
-  Image,
 } from 'react-native';
 
 const styles = require('../../styles/styles.ios');
+const InPageImage = require('../inPageImage');
+const AnimalText = require('../animalText');
 
-var Dimensions = require('Dimensions');
-var WINDOW_WIDTH = Dimensions.get('window').width;
-var WINDOW_HEIGHT = Dimensions.get('window').height;
+const IMAGES = [
+  require('../../images/animals/medvedKamcatsky/image00.png'),
+  require('../../images/animals/medvedKamcatsky/image01.png'),
+  require('../../images/animals/medvedKamcatsky/image02.png'),
+];
 
-const IMG_01 = require('../../images/animals/medvedKamcatsky/image00.png');
-const IMG_02 = require('../../images/animals/medvedKamcatsky/image01.png');
-const IMG_03 = require('../../images/animals/medvedKamcatsky/image02.png');
+const THUMBNAILS = [
+  require('../../images/animals/medvedKamcatsky/image00.png'),
+  require('../../images/animals/medvedKamcatsky/image01.png'),
+  require('../../images/animals/medvedKamcatsky/image02.png'),
+];
 
 var AnimalDetail = React.createClass({
-  renderCarousel: function() {
-    return (
-      // initialPage parameter
-      <Swiper showsButtons={true} style={{ width: WINDOW_WIDTH, height: WINDOW_WIDTH }}>
-      <View style={{backgroundColor:'#6C7A89',flex: 1}}>
-        <Image
-          style={{flex: 1, width: WINDOW_WIDTH, height: WINDOW_HEIGHT}}
-          resizeMode="contain"
-          source={IMG_01}
-        />
-      </View>
-      <View style={{backgroundColor:'yellow',flex: 1}}>
-        <Image
-          style={{flex: 1, width: WINDOW_WIDTH, height: WINDOW_HEIGHT}}
-          resizeMode="contain"
-          source={IMG_02}
-        />
-      </View>
-      <View style={{backgroundColor:'green',flex: 1}}>
-        <Image
-          style={{flex: 1, width: WINDOW_WIDTH, height: WINDOW_HEIGHT}}
-          resizeMode="contain"
-          source={IMG_03}
-        />
-      </View>
-      </Swiper>
-    );
+  componentWillMount() {
+    this.props.bg();
   },
 
   render() {
     return (
-  <View style={styles.vcontainer}>
-  <ScrollView
-    style={styles.scrollView}
-  >
-  <Text style={styles.ctext}>
-  Medvěd kamčatský (lat. Ursus arctos beringianus) je největší euroasijská suchozemská šelma, a jak už sám název napovídá, pochází z Kamčatky, poloostrova na Dálném východě v Rusku. Vyskytuje se také na přilehlých ostrovech a poloostrovech (Sachalin, Mandžusko). V České republice ho můžeme najít jen v naší zoo. Příroda, ze které k nám přicestoval, je skutečně krásná – většinu jejího povrchu tvoří vulkány, sopky, gejzíry a rozsáhlé lesy.
-  </Text>
-  <Image
-    source={IMG_01}
-    resizeMode='contain'
-    style={{
-      height: 100,
-      width: 300,
-    }}
-  />
-  <Text style={styles.ctext}>
-  Medvěd kamčatský, extrémně silný a odolný poddruh medvěda hnědého, dorůstá výšky až 130 cm v kohoutku a délky až 280 cm. Vážit může až 750 kg. Má rychlé reakce a je mrštný, jeho zuby dokážou překousnout jakoukoli kost, svými tlapami zvládá valit balvany, vyšplhá téměř na jakkoli kolmý svah i do koruny silného stromu a vydrží dlouhé hodiny sedět v ledové vodě. Je také vynikajícím plavcem a potápěčem. Na krátké vzdálenosti dosahuje v běhu rychlosti až 45–60 km/h a za den dokáže ujít 100 kilometrů. Fascinující je i jeho orientační smysl – za potravou putuje stovky kilometrů a pak se bez jediného zaváhání vrátí zpět do své domovské oblasti.  Je tedy pochopitelné, že na souši nemá kromě člověka přirozeného nepřítele. Ve volné přírodě se medvěd kamčatský dožívá 20 až 30 let, v lidské péči až 50 let.
-  </Text>
-  <Image
-    source={IMG_02}
-    resizeMode='contain'
-    style={{
-      height: 100,
-      width: 300,
-    }}
-  />
-  <Text style={styles.ctext}>
-  Jelikož je medvěd kamčatský všežravec, jeho strava je pestrá – má rád borůvky, brusinky, semena jasanu a borovice a díky svým schopnostem uloví i velkou rybu. Při nedostatku potravy však nepohrdne ani mršinami či trávou. V krajních případech se dokáže uchýlit i ke kanibalismu.
-  </Text>
-  <Text style={styles.ctext}>
-  Co se týče povahy tohoto na první pohled nepřemožitelného zvířete, medvěd kamčatský je samotář. Společnost vyhledává pouze v období páření. Délka březosti samice medvěda kamčatského se pohybuje v rozmezí od 6 do 8 měsíců v závislosti na podmínkách pro přežití mláďat.
-  </Text>
-  <Image
-    source={IMG_03}
-    resizeMode='contain'
-    style={{
-      height: 100,
-      width: 300,
-    }}
-  />
-  <Text style={styles.ctext}>
-  Zoo Brno chová v současnosti samce Jelizara a samici Kamčatku, kteří spolu zplodili již tři mláďata. Samečci Kuba a Toby se narodili 30. ledna 2012 a v prosinci roku 2014 odcestovali do soukromé ukrajinské zoo. V současné době se svými rodiči sdílí výběh i sameček Bruno, který se narodil 29. ledna 2016.
-  </Text>
-  </ScrollView>
-  </View>
-);
-}
+      <ScrollView>
+        <View>
+          <InPageImage firstImage={true} indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+        </View>
+        <View style={styles.scrollView}>
+          <AnimalText>
+            Medvěd kamčatský (lat. Ursus arctos beringianus) je největší euroasijská suchozemská šelma, a jak už sám název napovídá, pochází z Kamčatky, poloostrova na Dálném východě v Rusku. Vyskytuje se také na přilehlých ostrovech a poloostrovech (Sachalin, Mandžusko). V České republice ho můžeme najít jen v naší zoo. Příroda, ze které k nám přicestoval, je skutečně krásná – většinu jejího povrchu tvoří vulkány, sopky, gejzíry a rozsáhlé lesy.
+          </AnimalText>
+          <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+          <AnimalText>
+            Medvěd kamčatský, extrémně silný a odolný poddruh medvěda hnědého, dorůstá výšky až 130 cm v kohoutku a délky až 280 cm. Vážit může až 750 kg. Má rychlé reakce a je mrštný, jeho zuby dokážou překousnout jakoukoli kost, svými tlapami zvládá valit balvany, vyšplhá téměř na jakkoli kolmý svah i do koruny silného stromu a vydrží dlouhé hodiny sedět v ledové vodě. Je také vynikajícím plavcem a potápěčem. Na krátké vzdálenosti dosahuje v běhu rychlosti až 45–60 km/h a za den dokáže ujít 100 kilometrů. Fascinující je i jeho orientační smysl – za potravou putuje stovky kilometrů a pak se bez jediného zaváhání vrátí zpět do své domovské oblasti.  Je tedy pochopitelné, že na souši nemá kromě člověka přirozeného nepřítele. Ve volné přírodě se medvěd kamčatský dožívá 20 až 30 let, v lidské péči až 50 let.
+          </AnimalText>
+          <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+          <AnimalText>
+            Jelikož je medvěd kamčatský všežravec, jeho strava je pestrá – má rád borůvky, brusinky, semena jasanu a borovice a díky svým schopnostem uloví i velkou rybu. Při nedostatku potravy však nepohrdne ani mršinami či trávou. V krajních případech se dokáže uchýlit i ke kanibalismu.
+          </AnimalText>
+          <AnimalText>
+            Co se týče povahy tohoto na první pohled nepřemožitelného zvířete, medvěd kamčatský je samotář. Společnost vyhledává pouze v období páření. Délka březosti samice medvěda kamčatského se pohybuje v rozmezí od 6 do 8 měsíců v závislosti na podmínkách pro přežití mláďat.
+          </AnimalText>
+          <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+          <AnimalText>
+            Zoo Brno chová v současnosti samce Jelizara a samici Kamčatku, kteří spolu zplodili již tři mláďata. Samečci Kuba a Toby se narodili 30. ledna 2012 a v prosinci roku 2014 odcestovali do soukromé ukrajinské zoo. V současné době se svými rodiči sdílí výběh i sameček Bruno, který se narodil 29. ledna 2016.
+          </AnimalText>
+        </View>
+      </ScrollView>
+    );
+  }
 });
 
 module.exports = AnimalDetail;
--- components/animals/tygrSumatersky.child.js
+++ components/animals/tygrSumatersky.child.js
@@ -7,6 +7,7 @@ import {
 
 const styles = require('../../styles/styles.ios');
 const InPageImage = require('../inPageImage');
+const AnimalText = require('../animalText');
 
 const IMAGES = [
   require('../../images/animals/tygrSumatersky/image01.png'),
@@ -32,26 +33,26 @@ var AnimalDetail = React.createClass({
   <InPageImage firstImage={true} indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
 </View>
 <View style={styles.scrollView}>
-  <Text style={styles.ctext}>
+  <AnimalText>
   Ahoj! Jmenuji se Satu a v Brně žiji od roku 2005. Narodila jsem se v zoo v Dublinu, což je město v Irsku. Nejsem jen tak obyčejná tygřice, můj druh pochází se Sumatry. To je ostrov mezi Asií a Austrálií.
-  </Text>
+  </AnimalText>
   <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
-  <Text style={styles.ctext}>
+  <AnimalText>
   My tygři sumaterští žijeme obvykle v lesích, rašeliništích a bažinách. Už nás ale není moc. Míst, kde se nám dobře žije, totiž ubývá. Z lesů se stávají plantáže a my nemáme kde žít a lovit. Aby toho nebylo málo, najdou se i lidé, kteří nás zabíjejí kvůli kůži a drápům.
 
   Ačkoliv dospělí samci dosahují váhy pouhých 100 až 150 kilogramů a samice ještě méně, nelze tyto šelmy podceňovat. Tygři sumaterští – stejně jako všichni ostatní – jsou velmi hbití a silní. Vždyť ročně musí ulovit kolem 50 kusů preferované kořisti, kam patří například divoká prasata, jelenovití, někteří primáti, ale i ptáci, ryby, hlodavci a plazi.
-  </Text>
+  </AnimalText>
   <InPageImage indexes={[1,2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
-  <Text style={styles.ctext}>
+  <AnimalText>
   Každý z nás potřebuje své vlastní území, kde žije dostatek jiných zvířat. Jako všichni tygři i my se živíme lovem. Nejradši máme divoká prasata a jeleny, ale pochutnáme si i na ptácích a rybách. Umíme běhat mnohem rychleji než vy lidé, zvládneme i rychlost vyšší než 50 km/h – takže bychom dokázali předhonit například auto v obci. I když jsme menší než ostatní tygři, musíme každý rok doběhnout a ulovit alespoň 50 zvířat, abychom neměli hlad a mohli uživit mláďata.
-  </Text>
-  <Text style={styles.ctext}>
+  </AnimalText>
+  <AnimalText>
   O malá tygřata se staráme my samice. Když naše děti vyrostou a jsou jim přibližně dva roky, odcházejí od nás a hledají si svá vlastní území.
-  </Text>
+  </AnimalText>
   <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
-  <Text style={styles.ctext}>
+  <AnimalText>
   Tady v zoo nejsem sama. Sousední výběh obývá Dandys, který za mnou přijel z Varšavy. Moji chovatelé stejně jako já doufají, že s Dandysem budu mít malá tygřátka. Tak nám držte palce!
-  </Text>
+  </AnimalText>
 </View>
 </ScrollView>
     );
