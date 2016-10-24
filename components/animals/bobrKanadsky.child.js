d32dc02858dde14053ffd7c5133cdacd4b6f4890
Majuskaaa <ma.i.a@seznam.cz>
2016-10-24 17:06:04 +0200

    Update bobrKanadsky.child.js

--- components/animals/bobrKanadsky.child.js
+++ components/animals/bobrKanadsky.child.js
@@ -37,33 +37,30 @@ var AnimalDetail = React.createClass({
         Ahoj kamarádi,
         </AnimalText>
         <AnimalText>
-        my jsme bobři kanadští a tady v Bystrci bydlíme od roku 2003. Přivezli nás sem ze Zooparku v Chomutově a celkově nás přijelo pět. Pocházíme až ze Severní Ameriky, což je odsud hodně daleko, až za oceánem.
+        my jsme bobři kanadští a tady v Bystrci bydlíme od roku 2003. Přivezli nás sem ze Zooparku v Chomutově a celkově nás přijelo pět. Pocházíme ze Severní Ameriky, což je odsud hodně daleko, až za oceánem.
         </AnimalText>
         <AnimalText>
-        Naše tělo je pokryto hnědým až černým kožichem a je včetně ocásku dlouhé zhruba jako tělo malého koníka. Tak možná proto se o nás říká, že jsme třetí největší hlodavci na světě, což taky jsme!
+        Naše tělo je pokryto hnědým až černým kožichem a včetně ocásku měří až metr a půl. Možná právě proto se o nás říká, že jsme třetí největší hlodavci na světě! Přesně tak, jsme hlodavci stejně jako například křeček nebo veverka. Chvilkami si však připadáme spíše jako rybičky.
         </AnimalText>
-        <AnimalText>
-        Nejraději plaveme ve vodě. A víte, proč se nikdy z máčení ve vodě nenachladíme? To díky tomu, že naše srst nepropouští vodu. Při plavání si pomáháme ocasem, který nám slouží jako pádlo, a na zadních nohách máme navíc plovací blány. Jsme hlodavci, jako je například křeček nebo veverka, ale ve vodě si chvilkami připadáme spíše jako rybičky.
-        </AnimalText>
-        <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
         Proč rybičky? Nejraději ze všeho totiž plaveme. Z dlouhého máčení se ve vodě však nikdy nenachladneme. To díky tomu, že naše srst nepropouští vodu. Při plavání si pomáháme ocasem, který nám slouží jako pádlo, a na zadních nohách máme plovací blány.
         </AnimalText>
+        <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
-        Kromě plavání také rádi skotačíme v proudu řek, brousíme si zuby o dřevo,  stavíme hráze ze stromů anebo si hrajeme v našem bobřím hradě.  Říkáte si, bobři mají hrad? Ano, z větviček a hlíny.
+        Kromě plavání také rádi skotačíme v proudu řek, brousíme si zuby o dřevo,  stavíme hráze ze stromů nebo si hrajeme ve svém bobřím hradě.  Říkáte si, bobři mají hrad? Ano, z větviček a hlíny!
         </AnimalText>
         <AnimalText>
-        A zajímá vás, jestli míváme také bobří mláďátka? Samozřejmě, každý rok se nám narodí zhruba pět miminek (naposledy to byla 4 mláďata na konci června 2016), která si buď odvezou do jiných zoologických zahrad, anebo zůstávají tady s námi. Jednou si naše mláďátko odvezli dokonce až do Jižní Koreje!
+        Zajímá vás, jestli míváme také bobří mláďátka? Samozřejmě, každý rok se nám narodí zhruba pět miminek (naposledy to byla 4 mláďata na konci června 2016), která buď odcestují do jiných zoologických zahrad, nebo zůstávají tady s námi. Jednou si jedno naše mláďátko odvezli dokonce až do Jižní Koreje!
         </AnimalText>
         <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
         Rádi mlsáme kůru a pupeny vrby, mňam, jen jak to říkáme, hned bychom si nějaký ten pupen dali!
         </AnimalText>
         <AnimalText>
-        Tak se mějte krásně a zase nás přijďte navštívit,
+        Mějte se krásně a zase nás přijďte navštívit,
         </AnimalText>
         <AnimalText>
-        pac a bobří pusu.
+        pac a bobří pusu!
         </AnimalText>
         </View>
       </ScrollView>
