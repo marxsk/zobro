c856d1ea84f55a203602f8156a8e2c5e0f2f7c97
Majuskaaa <ma.i.a@seznam.cz>
2016-11-12 16:23:15 +0100

    Update lachtanMedvedi.child.js

--- components/animals/lachtanMedvedi.child.js
+++ components/animals/lachtanMedvedi.child.js
@@ -22,7 +22,7 @@ const THUMBNAILS = [
 var AnimalDetail = React.createClass({
   render() {
     return (
-      <AnimalTemplate firstIndex={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator}>
+      <AnimalTemplate firstIndex={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator}>
           <AnimalText>
             Zdravím, člověčí mládě!
           </AnimalText>
@@ -32,11 +32,11 @@ var AnimalDetail = React.createClass({
           <AnimalText>
             Od ostatních druhů lachtanů mě můžeš rozeznat díky mým velkým očím a uším. Velká ouška mám proto, aby mi do nich nezatekla voda, když plavu. Taky mám velkou zadní ploutev, která mi pomáhá při plavání. Schválně, jak dlouho vydržíš pod vodou? Já až sedm minut! Ale to dělám, jenom když si hraju s&nbsp;kamarády, normálně se pod vodou schovávám kolem dvou a půl minuty.
           </AnimalText>
-          <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+          <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
           <AnimalText>
             Kluky od holek lachtanů medvědích rozeznáš celkem snadno. V&nbsp;dospělosti mají holky okolo 150&nbsp;cm, což je asi jako celá tvoje postýlka, a váží kolem 50&nbsp;kg. To je o&nbsp;něco míň, než kolik váží dospělá člověčí holka, nebo si to můžeš představit jako obrovský pytel brambor. Zato já budu větší! Až budu dospělý, budu vážit až 270&nbsp;kg, to je jako tři a půl průměrných dospělých pánů dohromady!
           </AnimalText>
-          <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+          <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
           <AnimalText>
             Abych pořádně vyrostl, musím hodně jíst. Denně zvládnu spořádat až 5&nbsp;kg ryb. Každý den dostávám sledě, makrely a jako pochoutku kalamáry.
           </AnimalText>
