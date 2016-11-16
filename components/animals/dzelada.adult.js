af865223490d5a919da0b264c15ec8c4c9081f2e
Kajjulka <klaravostrelova@seznam.cz>
2016-11-16 23:54:32 +0100

    Update dzelada.adult.js

--- components/animals/dzelada.adult.js
+++ components/animals/dzelada.adult.js
@@ -28,14 +28,14 @@ var AnimalDetail = React.createClass({
         <AnimalText>
           Průměrná délka života v&nbsp;zajetí bývá 30&nbsp;let, v&nbsp;přírodě se odhaduje na 24&nbsp;let. Volně se vyskytují jen na horských loukách etiopských skalnatých masivů. Jsou na toto prostředí dokonale adaptované, noci tráví schoulené na okraji strmých svahů. Může se to sice jevit nebezpečně, ale velkou výhodou je, že se k&nbsp;nim nedostanou téměř žádní predátoři. Vysokohorskému prostředí se dželady přizpůsobily mimo jiné i&nbsp;svým pozadím, které je velmi podobné tomu lidskému. Sedací partie těchto savců jsou ideálně uzpůsobeny vysedávání na ostrých skalách. Evoluční biologové uvažují, zda mohly v&nbsp;podobných podmínkách vzniknout i&nbsp;naše sedací partie.
         </AnimalText>
-      <AnimalTemplate firstIndex={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+      <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
           Po zemi se pohybují po všech čtyřech končetinách. Jejich protistojný palec jim pomáhá při hledání a zpracování potravy, umožňuje dokonalé třídění trávy a travních semen. Krátké a robustní prsty jim zároveň umožňují v&nbsp;období sucha vykopávat hlízy a kořínky. Krmí se vsedě, tudíž mají obě ruce volné. Jednou rukou třídí a v&nbsp;druhé shromažďují potravu, dokud si nevytvoří sousto vhodné ke žvýkání a spolknutí.
         </AnimalText>
         <AnimalText>
           U&nbsp;těchto opic také můžeme pozorovat různé expresivní výrazy, například přivírání očních víček k&nbsp;vyjádření dominance. Při pocitu strachu či ohrožení ohrnují horní ret až téměř k&nbsp;nosu, vyzývají tak také oponenta ke konfliktní situaci. Mezi další osvojenou mimiku patří zívání, upřené pohledy, strnulé zírání, a dokonce pohyby rtů. Komunikují spolu i&nbsp;akusticky, tedy pomocí varovných signálů jako například vřískání, mručení či štěkání. Pachovou komunikaci užívají v&nbsp;průběhu páření samice. Hmatová komunikace je určena většinou k&nbsp;posílení sociálních vztahů. Jedinci si navzájem probírají srst či jako jistou formu pozdravu třou nosy o&nbsp;sebe. Stejně jako většina primátů používají fyzický kontakt také při péči o&nbsp;mláďata.
         </AnimalText>
-      <AnimalTemplate firstIndex={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+      <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
           Další zajímavostí je fakt, že samci tohoto druhu jsou sice větší a silnější, ale samice jsou dominantnější. Pokud u&nbsp;samec na svou roli v&nbsp;tlupě nestačí, vyberou si samice jeho následníka. Tito samci, kteří o&nbsp;své místo v&nbsp;tlupě přišli, nebo na něj naopak teprve čekají, si vytváří vlastní, samčí skupiny. S&nbsp;rodinami se pak často spojují do větších tlup o&nbsp;mnoha desítkách členů. Zatím největší pozorovaná skupina měla 670 členů.
         </AnimalText>
