2a4f95625c3b173ef46db0eac75e4c9b8687b4c7
Kajjulka <klaravostrelova@seznam.cz>
2016-11-16 23:42:13 +0100

    Update klokanZlutonohy.child.js

--- components/animals/klokanZlutonohy.child.js
+++ components/animals/klokanZlutonohy.child.js
@@ -28,11 +28,11 @@ var AnimalDetail = React.createClass({
         <AnimalText>
           jsem tu jen na skok. Jmenuji se Moonie a stejně jako moje starší kamarádka Corowa jsem do Brna přijela v&nbsp;roce 2015 z&nbsp;francouzské zoo v&nbsp;Mulhouse. V&nbsp;květnu 2016 se k&nbsp;nám připojil i&nbsp;Thutamba, tehdy roční sameček z Bristolu, města v Anglii. Říkají nám klokani žlutonozí, nejspíš proto, že máme přední stranu nohou žlutočerveně zbarvenou. Podobně barevné jsou i&nbsp;naše ocasy, ale mají na sobě navíc tmavé pruhy. Ocas je naší největší chloubou, může být dokonce delší než celé naše tělo. Nemáme ho ale jen na ozdobu, pomáhá nám udržovat rovnováhu. Hlavně při doskoku je to neocenitelný pomocník. A když nám zrovna není do skoku, složíme si ho pěkně za záda, a proto nám vůbec nepřekáží.
         </AnimalText>
-      <AnimalTemplate firstIndex={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator}>        
+      <AnimalTemplate firstIndex={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />        
         <AnimalText>
           Jako každý správný klokan původně pocházíme z&nbsp;Austrálie, hlavně z&nbsp;její jižní a střední části. Řadíme se ke klokanům skalním, jelikož vyhledáváme kamenité svahy a prudké srázy. Na život v&nbsp;takových podmínkách jsme dobře připraveni. Na zadních nohách máme drsné polštářky porostlé hustými chlupy, aby nám to neklouzalo, a dráp čtvrtého prstu máme kratší než ostatní klokani. Dokážeme dokonce i&nbsp;vylézt na strom a při té příležitosti si pochutnat na listech nebo pupenech, většinou si ale vystačíme s&nbsp;obyčejnou trávou.
         </AnimalText>
-      <AnimalTemplate firstIndex={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator}>        
+      <AnimalTemplate firstIndex={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />        
         <AnimalText>
           Když o&nbsp;tom tak přemýšlím, asi si skočím pro něco k&nbsp;snědku. Tak se mějte!
         </AnimalText>
