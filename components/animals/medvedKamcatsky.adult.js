7cd67781cf50ca5628c991cca007a9c5a451b44d
Kajjulka <klaravostrelova@seznam.cz>
2016-11-17 00:04:17 +0100

    Update medvedKamcatsky.adult.js

--- components/animals/medvedKamcatsky.adult.js
+++ components/animals/medvedKamcatsky.adult.js
@@ -28,14 +28,14 @@ var AnimalDetail = React.createClass({
           <AnimalText>
             Medvěd kamčatský, extrémně silný a odolný poddruh medvěda hnědého, dorůstá výšky až 130&nbsp;cm v&nbsp;kohoutku a délky až 280&nbsp;cm. Vážit může až 750&nbsp;kg. Má rychlé reakce a je mrštný, jeho zuby dokážou překousnout jakoukoli kost, svými tlapami zvládá valit balvany, vyšplhá téměř na jakkoli kolmý svah i&nbsp;do koruny silného stromu a vydrží dlouhé hodiny sedět v&nbsp;ledové vodě. Je také vynikajícím plavcem a potápěčem. Na krátké vzdálenosti dosahuje v&nbsp;běhu rychlosti až 45–60&nbsp;km/h a za den dokáže ujít 100&nbsp;km. Fascinující je i&nbsp;jeho orientační smysl – za potravou putuje stovky kilometrů a pak se bez jediného zaváhání vrátí zpět do své domovské oblasti. Je tedy pochopitelné, že na souši nemá kromě člověka přirozeného nepřítele. Ve volné přírodě se medvěd kamčatský dožívá 20 až 30 let, v&nbsp;lidské péči až 50 let.
           </AnimalText>
-          <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+        <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
           <AnimalText>
             Jelikož je medvěd kamčatský všežravec, jeho strava je pestrá – má rád borůvky, brusinky, semena jasanu a borovice a díky svým schopnostem uloví i&nbsp;velkou rybu. Nepohrdne však ani mršinami či trávou. Zjednodušeně řečeno sní, na co přijde.
           </AnimalText>
           <AnimalText>
             Co se týče povahy tohoto na první pohled nepřemožitelného zvířete, medvěd kamčatský je samotář. Společnost vyhledává pouze v&nbsp;období páření. Délka březosti samice medvěda kamčatského se pohybuje v&nbsp;rozmezí od 6 do 8 měsíců v&nbsp;závislosti na podmínkách pro přežití mláďat.
           </AnimalText>
-          <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+        <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
           <AnimalText>
             Zoo Brno chová v&nbsp;současnosti samce Jelizara a samici Kamčatku, kteří spolu zplodili již tři mláďata. Samečci Kuba a Toby se narodili 30.&nbsp;ledna 2012 a v&nbsp;prosinci roku 2014 odcestovali do soukromé ukrajinské zoo. V&nbsp;současné době se svými rodiči sdílí výběh i&nbsp;sameček Bruno, který se narodil 29.&nbsp;ledna 2016.
           </AnimalText>
