34398168e8a40699011497393f255ff257997348
Kajjulka <klaravostrelova@seznam.cz>
2016-11-16 23:57:27 +0100

    Update bobrKanadsky.child.js

--- components/animals/bobrKanadsky.child.js
+++ components/animals/bobrKanadsky.child.js
@@ -31,7 +31,7 @@ var AnimalDetail = React.createClass({
         <AnimalText>
           Naše tělo je pokryto hnědým až černým kožichem a včetně ocásku měří až metr a půl. Možná právě proto se o&nbsp;nás říká, že jsme třetí největší hlodavci na světě! Přesně tak, jsme hlodavci stejně jako například křeček nebo veverka. Chvilkami si však připadáme spíše jako rybičky.
         </AnimalText>
-        <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+      <InPageImage indexes={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
           Proč rybičky? Nejraději ze všeho totiž plaveme. Z&nbsp;dlouhého máčení se ve vodě však nikdy nenachladneme. To díky tomu, že naše srst nepropouští vodu. Při plavání si pomáháme ocasem, který nám slouží jako pádlo, a na zadních nohách máme plovací blány.
         </AnimalText>
@@ -41,7 +41,7 @@ var AnimalDetail = React.createClass({
         <AnimalText>
           Zajímá vás, jestli míváme také bobří mláďátka? Samozřejmě, každý rok se nám narodí zhruba pět miminek (naposledy to byla 4&nbsp;mláďata na konci června 2016), která buď odcestují do jiných zoologických zahrad, nebo zůstávají tady s&nbsp;námi. Jednou si jedno naše mláďátko odvezli dokonce až do Jižní Koreje!
         </AnimalText>
-        <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
+      <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
           Rádi mlsáme kůru a pupeny vrby, mňam, jen jak to říkáme, hned bychom si nějaký ten pupen dali!
         </AnimalText>
