de0162be41e5909b8f869e4e0db8f3ff8f1d15fe
Majuskaaa <ma.i.a@seznam.cz>
2017-01-13 14:58:48 +0100

    Update dzelada.adult.js

--- components/animals/dzelada.adult.js
+++ components/animals/dzelada.adult.js
@@ -23,14 +23,14 @@ var AnimalDetail = React.createClass({
     return (
       <AnimalTemplate firstIndex={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator}>
         <AnimalText>
-          Dželada je opice, která je velmi podobná paviánům. Od nich se však liší červenou lysinou na prsou. Díky tomuto charakteristickému znaku se dželadám přezdívá „pavián s&nbsp;krvácejícím srdcem“.
+          Dželada je opice, která je blízká příbuzná paviánů. Od nich se však liší červenou lysinou na prsou. Díky tomuto charakteristickému znaku se dželadám přezdívá „pavián s&nbsp;krvácejícím srdcem“.
         </AnimalText>
         <AnimalText>
           Průměrná délka života v&nbsp;zajetí bývá 30&nbsp;let, v&nbsp;přírodě se odhaduje na 24&nbsp;let. Volně se vyskytují jen na horských loukách etiopských skalnatých masivů. Jsou na toto prostředí dokonale adaptované, noci tráví schoulené na okraji strmých svahů. Může se to sice jevit nebezpečně, ale velkou výhodou je, že se k&nbsp;nim nedostanou téměř žádní predátoři. Vysokohorskému prostředí se dželady přizpůsobily mimo jiné i&nbsp;svým pozadím, které je velmi podobné tomu lidskému. Sedací partie těchto savců jsou ideálně uzpůsobeny vysedávání na ostrých skalách. Evoluční biologové uvažují, zda mohly v&nbsp;podobných podmínkách vzniknout i&nbsp;naše sedací partie.
         </AnimalText>
       <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
-          Po zemi se pohybují po všech čtyřech končetinách. Jejich protistojný palec jim pomáhá při hledání a zpracování potravy, umožňuje dokonalé třídění trávy a travních semen. Krátké a robustní prsty jim zároveň umožňují v&nbsp;období sucha vykopávat hlízy a kořínky. Krmí se vsedě, tudíž mají obě ruce volné. Jednou rukou třídí a v&nbsp;druhé shromažďují potravu, dokud si nevytvoří sousto vhodné ke žvýkání a spolknutí.
+          Po zemi se pohybují po všech čtyřech končetinách. Jejich protistojný palec jim pomáhá při hledání a zpracování potravy, umožňuje dokonalé třídění trávy a travních semen. Krátké a robustní prsty jim zároveň umožňují v&nbsp;období sucha vyhrabávat hlízy a kořínky. Krmí se vsedě, tudíž mají obě ruce volné. Jednou rukou třídí a v&nbsp;druhé shromažďují potravu, dokud si nevytvoří sousto vhodné ke žvýkání a spolknutí.
         </AnimalText>
         <AnimalText>
           U&nbsp;těchto opic také můžeme pozorovat různé expresivní výrazy, například přivírání očních víček k&nbsp;vyjádření dominance. Při pocitu strachu či ohrožení ohrnují horní ret až téměř k&nbsp;nosu, vyzývají tak také oponenta ke konfliktní situaci. Mezi další osvojenou mimiku patří zívání, upřené pohledy, strnulé zírání, a dokonce pohyby rtů. Komunikují spolu i&nbsp;akusticky, tedy pomocí varovných signálů jako například vřískání, mručení či štěkání. Pachovou komunikaci užívají v&nbsp;průběhu páření samice. Hmatová komunikace je určena většinou k&nbsp;posílení sociálních vztahů. Jedinci si navzájem probírají srst či jako jistou formu pozdravu třou nosy o&nbsp;sebe. Stejně jako většina primátů používají fyzický kontakt také při péči o&nbsp;mláďata.
