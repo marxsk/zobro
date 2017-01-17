ee5196628e7eecaa6200299ad4e6c64ac3fe4eae
Majuskaaa <ma.i.a@seznam.cz>
2017-01-17 10:09:54 +0100

    Update jespakBojovny.adult.js

--- components/animals/jespakBojovny.adult.js
+++ components/animals/jespakBojovny.adult.js
@@ -30,7 +30,7 @@ var AnimalDetail = React.createClass({
         </AnimalText>
         <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
-          Samci samicím přerůstají přes hlavu, obvykle o&nbsp;nějakých 5&nbsp;až 8&nbsp;centimetrů, jinak se však příliš neliší. Všichni mají hnědé peří s&nbsp;černými skvrnami, bílé bříško a dlouhý špičatý zobák. Když ovšem chtějí samci v&nbsp;období páření (květen až červen) zapůsobit na samice, to pak hrají všemi barvami. Tedy všemi ne, ale naroste jim pestrý vztyčitelný límec (hnědý, žlutý až bílý), tváře si napudrují červenožlutou pudřenkou a tou dobou mají za ušima (kudrnaté chocholky). Sem tam připojí i&nbsp;nějakou tu poklonu. Chtějí za každou cenu ubránit svou pozici na tokaništi, a tak vám v&nbsp;tomto období nabídnou náramnou podívanou v podobě soubojů.
+          Samci samicím přerůstají přes hlavu, obvykle o&nbsp;nějakých 5&nbsp;až 8&nbsp;centimetrů, jinak se však příliš neliší. Všichni mají hnědé peří s&nbsp;černými skvrnami, bílé bříško a dlouhý špičatý zobák. Když ovšem chtějí samci v&nbsp;období páření (květen až červen) zapůsobit na samice, to pak hrají všemi barvami. Tedy všemi ne, ale naroste jim pestrý vztyčitelný límec (hnědý, žlutý až bílý), tváře si napudrují červenožlutou pudřenkou a tou dobou mají za ušima (chocholky). V období toku mají tak variabilní zbarvení, že se údajně nenajdou dva úplně stejní samci. Sem tam připojí i&nbsp;nějakou tu poklonu. Chtějí za každou cenu ubránit svou pozici na tokaništi, a tak vám v&nbsp;tomto období nabídnou náramnou podívanou v podobě soubojů.
         </AnimalText>
         <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
         <AnimalText>
