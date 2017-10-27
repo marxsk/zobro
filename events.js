9ab866a95142a90c73c16edbc6a1b471af444993
Majuskaaa <ma.i.a@seznam.cz>
2017-10-27 10:43:20 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,4 +1,13 @@
 export default events = [
+  { id: 'ledved1030', startDate: '2017-11-01', endDate: '2018-03-14', name: 'Medvěd lední', time: '10:30', weekdays: [5,6], place: 'Expozice ledních medvědů', thumbnail: require('./images/animals/medvedLedni/01-thumb.jpg'), text: 'Lorem ipsum'},
+  { id: 'panda1100', startDate: '2017-11-01', endDate: '2018-03-14', name: 'Panda červená', time: '11:00', weekdays: [5,6], place: 'Expozice pandy červené', thumbnail: require('./images/animals/pandaCervena/01-thumb.jpg'), text: 'Lorem ipsum'},
+  { id: 'simpanz1130', startDate: '2017-11-01', endDate: '2018-03-14', name: 'Šimpanz', time: '11:30', weekdays: [5], place: 'Pavilon opic', thumbnail: require('./images/animals/simpanz/01-thumb.jpg'), text: 'Lorem ipsum'},
+  { id: 'lev1400', startDate: '2017-11-01', endDate: '2018-03-14', name: 'Lev konžský', time: '14:00', weekdays: [5,6], place: 'Expozice lvů konžských', thumbnail: require('./images/animals/levKonzsky/01-thumb.jpg'), text: 'Lorem ipsum'},
+  { id: 'drapkate1430', startDate: '2017-11-01', endDate: '2018-03-14', name: 'Drápkaté opičky, time: '14:30', weekdays: [5,6], place: 'Tropické království', thumbnail: require('./images/animals/kosman/01-thumb.jpg'), text: 'Lorem ipsum'},
+  { id: 'vari1130', startDate: '2017-11-01', endDate: '2018-03-14', name: 'Vari černobílý', time: '11:30', weekdays: [6], place: 'Pavilon opic', thumbnail: require('./images/animals/variCernobily/01-thumb.jpg'), text: 'Lorem ipsum'},
+
+
+  
   { id: 'ledved1030', startDate: '2017-03-15', endDate: '2017-09-30', name: 'Medvěd lední', time: '10:30', weekdays: [0,3], place: 'Expozice ledních medvědů', thumbnail: require('./images/animals/medvedLedni/01-thumb.jpg'), text: 'Lorem ipsum'},
   { id: 'ledved1130', startDate: '2017-03-15', endDate: '2017-09-30', name: 'Medvěd lední', time: '11:30', weekdays: [5], place: 'Expozice ledních medvědů', thumbnail: require('./images/animals/medvedLedni/01-thumb.jpg'), text: 'Lorem ipsum'},
   { id: 'kamcat1030', startDate: '2017-03-15', endDate: '2017-09-30', name: 'Medvěd kamčatský', time: '10:30', weekdays: [1,6], place: 'Beringie', thumbnail: require('./images/animals/medvedKamcatsky/01-thumb.jpg'), text: 'Lorem ipsum'},
